fdd7f01adc87b4a1ef78d1b95f1051491503826c
jiaulislam <jiaulislam.ict.bd@gmail.com>
2023-07-06 15:58:38 +0600

    :lipstick: added rick progressbar in export all

--- dumper/main.py
+++ dumper/main.py
@@ -4,17 +4,36 @@ from typing import Annotated
 import typer
 from config import ROOT_DIR
 from db import engine
+from rich.progress import track
 from schemas import Designation
 from utils import make_file_name, write_csv_file
 
 app = typer.Typer(name="Dumper")
 
 
-@app.command()
+@app.command(short_help="Dump Agents Number by providing Designation & Training Hour")
 def export(
     desg: Annotated[Designation, typer.Option()],
     hour: Annotated[int, typer.Option()] = 72,
 ):
+    """
+    Export Agents Number by providing Designation and Training Hour.
+
+    Parameters:
+    - desg (Designation): The designation of the agents.
+    - hour (int): The training hour (default: 72).
+
+    This command exports the numbers of agents based on the provided designation and training hour. It creates a CSV file with the agent numbers in the specified output directory.
+
+    Example usage:
+    ```
+    $ python main.py export --desg FA --hour 48
+    ```
+
+    Output:
+    - The command will generate a CSV file with the agent numbers in the output directory.
+    - It will print a message to the console confirming the file export.
+    """
     file_name = make_file_name(desg, hour)
 
     today = datetime.today()
@@ -31,8 +50,25 @@ def export(
         print(f"File: {file_name}.csv has been exported to output dir.")
 
 
-@app.command()
+@app.command(short_help="Dump All Agents Designation Number by providing Traning Hour")
 def export_all_desg(hour: Annotated[int, typer.Option()]):
+    """
+    Export All Agents Designation Number by providing Training Hour.
+
+    Parameters:
+    - hour (int): The training hour.
+
+    This command exports the designation numbers of all agents based on the provided training hour. It creates a CSV file for each designation in the specified output directory.
+
+    Example usage:
+    ```
+    $ python main.py export-all-desg --hour 0 | 36 | 72
+    ```
+
+    Output:
+    - The command will generate CSV files for each designation in the output directory.
+    - It will print the names of the exported files to the console.
+    """
     today = datetime.today()
 
     output_dir = ROOT_DIR / "output" / today.strftime("%d-%b-%Y")
@@ -41,7 +77,9 @@ def export_all_desg(hour: Annotated[int, typer.Option()]):
         output_dir.mkdir(parents=True, exist_ok=True)
 
     with engine.connect() as connection:
-        for desg in Designation.__members__.values():
+        for desg in track(
+            Designation.__members__.values(), description="Processing..."
+        ):
             file_name = make_file_name(desg, hour=hour)
             output_file = output_dir / f"{file_name}.csv"
             write_csv_file(output_file, desg, connection, hour)
